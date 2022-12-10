a97cd5c931790503be1c40c99b38963a69000abd
ArsenKoch <arsenkocaran@icloud.com>
2022-12-10 22:31:03 +0300

    Create first service

---
+++ app/src/main/java/com/example/services/MyService.kt
@@ -0,0 +1,49 @@
+package com.example.services
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.*
+
+class MyService : Service() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        coroutineScope.launch {
+            for (i in 0 until 100) {
+                delay(1000)
+                log("Timer: $i")
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineScope.cancel()
+        log("onDestroy")
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    private fun log(message: String) {
+        Log.d("MyService", "New message: $message")
+    }
+
+    companion object {
+
+        fun intent(context: Context): Intent {
+            return Intent(context, MyService::class.java)
+        }
+    }
+}
