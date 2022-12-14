966ca779fbf1c935d228220e1f66ed164e7884c3
ArsenKoch <arsenkocaran@icloud.com>
2022-12-14 13:06:16 +0300

    JobScheduler. Method enqueue()

--- app/src/main/java/com/example/services/MyJobService.kt
+++ app/src/main/java/com/example/services/MyJobService.kt
@@ -2,6 +2,8 @@ package com.example.services
 
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.content.Intent
+import android.os.Build
 import android.util.Log
 import kotlinx.coroutines.*
 
@@ -16,12 +18,20 @@ class MyJobService : JobService() {
 
     override fun onStartJob(p0: JobParameters?): Boolean {
         log("onStartJob")
-        coroutineScope.launch {
-            for (i in 0 until 100) {
-                delay(1000)
-                log("Timer: $i")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            coroutineScope.launch {
+                var workItem = p0?.dequeueWork()
+                while (workItem != null) {
+                    val page = workItem.intent.getIntExtra(PAGE, 0)
+                    for (i in 0 until 5) {
+                        delay(1000)
+                        log("Timer: $i $page")
+                    }
+                    p0?.completeWork(workItem)
+                    workItem = p0?.dequeueWork()
+                }
+                jobFinished(p0, true)
             }
-            jobFinished(p0, true)
         }
         return true
     }
@@ -44,5 +54,12 @@ class MyJobService : JobService() {
     companion object {
 
         const val JOB_ID = 112
+        const val PAGE = "page"
+
+        fun newIntent(page: Int): Intent {
+            return Intent().apply {
+                putExtra(PAGE, page)
+            }
+        }
     }
 }
